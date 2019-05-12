2fb41ae11502476c2b32211b85051426efc74aa3
tahuiguotoukan <493967208@qq.com>
2019-05-12 23:10:15 +0800

    改版

---
+++ moban3688/data.js
@@ -0,0 +1,82 @@
+let data = {
+	banner: [
+		'banner1.jpg',
+		'banner2.jpg',
+		'banner3.jpg'
+	],
+	about_us:[
+		'aboutUs1.jpg',
+		'aboutUs2.jpg',
+		'aboutUs3.jpg'
+	],
+	product:[
+		{
+			classImg: 'productClass1.jpg',
+			list:[
+				{
+					img: 'productClass1.jpg',
+					detailImg: [
+					
+					]
+				},
+				{
+					img: 'productClass1.jpg',
+					detailImg: [
+					
+					]
+				},
+				{
+					img: 'productClass1.jpg',
+					detailImg: [
+					
+					]
+				},
+				{
+					img: 'productClass1.jpg',
+					detailImg: [
+					
+					]
+				},
+				{
+					img: 'productClass1.jpg',
+					detailImg: [
+					
+					]
+				}
+			]
+		},
+		{
+			classImg: 'productClass2.jpg',
+			list:[
+				{
+					img: 'productClass2.jpg',
+					detailImg: [
+					
+					]
+				}
+			]
+		},
+		{
+			classImg: 'productClass3.jpg',
+			list:[
+				{
+					img: 'productClass3.jpg',
+					detailImg: [
+					
+					]
+				}
+			]
+		},
+		{
+			classImg: 'productClass4.jpg',
+			list:[
+				{
+					img: 'productClass4.jpg',
+					detailImg: [
+					
+					]
+				}
+			]
+		},
+	]
+}
